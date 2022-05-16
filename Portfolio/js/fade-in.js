7b21a1a967ce242539e929b8643303b947a964b0
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-05-16 12:40:02 +0900

    Delete Portfolio directory

--- Portfolio/js/fade-in.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$(".fade-object").on("inview", function (event, isInView) {
-		if (isInView) {
-			$(this).stop().addClass("show");
-		} else {
-			$(this).stop().removeClass("show");
-		}
-	});
-});
